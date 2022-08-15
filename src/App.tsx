b6a8c8b2926ea97632ab40c410d0a34955cefd48
arip995 <arindampanda2002@gmail.com>
2022-08-15 06:26:49 +0530

    removed svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import {Button} from "./stories/Button"
 function App() {
 	return (
 		<div className="App">
-			<h1 className="text-3xl text-red-300 font-bold underline">Hello world!</h1>
+			<Button label="Click Me!" primary={true} ></Button>
+			<h1 className={"text-3xl text-red-300 font-bold underline"}>Hello world!</h1>
 		</div>
 	);
 }
