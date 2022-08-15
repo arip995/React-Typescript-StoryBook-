44a5cc1e4807855e195e7ddb102bf99ef2f4e07c
arip995 <arindampanda2002@gmail.com>
2022-08-15 15:02:37 +0530

    total configured

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {Button} from "./stories/Button"
 function App() {
 	return (
 		<div className="App">
-			<Button label="Click Me!" primary={true} ></Button>
+			<Button primary={true} >Click Me!</Button>
 			<h1 className={"text-3xl text-red-300 font-bold underline"}>Hello world!</h1>
 		</div>
 	);
